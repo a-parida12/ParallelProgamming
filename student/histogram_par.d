40cbc1a32a2689571c9a8be4026356529db0b07c
a-parida12 <abhijeet.parida@tum.de>
2017-05-09 01:51:59 +0200

    messade

---
+++ student/histogram_par.d
@@ -0,0 +1,4 @@
+student/histogram_par.o: student/histogram_par.c \
+ /home/a_parida/Documents/Github/ParallelProgamming/histogram.h
+
+/home/a_parida/Documents/Github/ParallelProgamming/histogram.h:
