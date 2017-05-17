29835f4ef1a42da4bc238171c7530df076a981e7
Abhijeet Parida <parida@in.tum.de>
2017-05-17 14:57:49 +0200

    minor error count

--- WarAndPeace/student/histogram_par.c
+++ WarAndPeace/student/histogram_par.c
@@ -5,9 +5,10 @@
 #include <ctype.h>
 
 #include "histogram.h"
+int total_thread;
 
 struct pthread_args{
-	int total_thread;
+	
 	long thread_no;
 	int nBlocks;
 	block_t *blocks;
@@ -20,26 +21,22 @@ void* Checkword(void* ptr){
 	char current_word[20] = "";
 	int c = 0;
 	struct pthread_args *arg = (pthread_args *)ptr;
+	int nBlockThread=arg->nBlocks/(total_thread);
+
+	int start= arg->thread_no*nBlockThread;
+	int end;
+	if (arg->thread_no!=total_thread)
+		 end =arg->thread_no*nBlockThread+nBlockThread;
+	else
+		end= arg->nBlocks;
 	
-	int Thr_no=arg->thread_no;
-	int nBlocks=arg->nBlocks;
-	block_t *blocks=arg->blocks;
-	int* histogram=arg->histogram;
-
-
-	printf("Hello from Thread %d\n",Thr_no);
-	printf("block size %d\n",nBlocks);
-
-	int nBlockThread=nBlocks/(arg->total_thread);
-	printf("block size %d\n",nBlockThread);
-	for (int i = Thr_no*nBlockThread; i <Thr_no*nBlockThread+nBlockThread; i++) {
-		printf("i %d\n",i);
-		printf("%d,BLOCKSIZE",BLOCKSIZE);
-		for (int j = 0; j < BLOCKSIZE; j++) {
-			//printf("j %d\n",i);
+	for (int i = start; i < end; i++) {
+				
+		for (int j = 0; j < BLOCKSIZE;j++) {
+						
 			// if the character is an alphabet store it to form a word.
-			if(isalpha(blocks[i][j])){
-				current_word[c++] = blocks[i][j];
+			if(isalpha(arg->blocks[i][j])){
+				current_word[c++] = arg->blocks[i][j];
 			}
 
 			// if the character is not an alphabet it is the end of a word.
@@ -47,13 +44,17 @@ void* Checkword(void* ptr){
 			else {
 				current_word[c] = '\0';
 				for(int k = 0; k < NNAMES; k++)	{
-					if(!strcmp(current_word, names[k]))
-						histogram[k]++;
+					
+					if(!strcmp(current_word, names[k])){
+						arg->histogram[k]++;
+					}
 				}
+			c = 0;
 			}
+			
 		}
 	}
-	arg->histogram=histogram;
+	
 	return NULL;
 }
 
@@ -61,7 +62,7 @@ void* Checkword(void* ptr){
 
 void get_histogram(int nBlocks, block_t *blocks, int* histogram, int num_threads)
 {
-
+	total_thread=num_threads;
 	pthread_t *thread;
 	struct pthread_args *arg;
 	thread=(pthread_t*)malloc(num_threads * sizeof(*thread));
@@ -69,23 +70,19 @@ void get_histogram(int nBlocks, block_t *blocks, int* histogram, int num_threads
 	
 	
 	for (int i=0;i<num_threads; i++){
-		arg[i].total_thread=num_threads;
 		arg[i].thread_no=i;
 		arg[i].nBlocks=nBlocks;
 		arg[i].blocks=blocks;
 		arg[i].histogram=histogram;
-		printf("Thread created\n");
+		
 		pthread_create(thread+i,NULL,&Checkword,arg+i);
 	}
 
 	for (int i=0;i<num_threads; i++){
 		pthread_join(thread[i] , NULL);
-		printf("Thread Joined\n");
-		for(int k = 0; k < NNAMES; k++){
-			histogram[k]=histogram[k]+arg[i].histogram[k];
-		}
-
+		
 	}
+
 
 	free(thread);
 	free(arg);
