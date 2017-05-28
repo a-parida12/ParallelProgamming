77382fe91e702320f2fce64c089ec8bf02c860cf
a-parida12 <abhijeet.parida@tum.de>
2017-05-28 22:59:38 +0200

    ASsignment 2 Submitted

--- WarAndPeace2/student/histogram_par.c
+++ WarAndPeace2/student/histogram_par.c
@@ -5,76 +5,94 @@
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #include "names.h"
 
-int a=0;
+char *global_buffer;
+int residual;
+int curTask_index;
+int CHUNK;
 
-struct pthread_args{
+pthread_mutex_t mutex_variable = PTHREAD_MUTEX_INITIALIZER;
 
-	char *buffer;
-	int* histogram;
-		
-};
+void * ThreadWorker(void * arg) {
+	
+    int local_hist[NNAMES];
+    int* m_hist = (int*)arg;
+    int count = 0;
+    unsigned int start, end;
+    
+    for (int i = 0; i < NNAMES; i ++)
+        local_hist[i] = 0;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void* threadOperator(void* ptr){
-
-	struct pthread_args *thread_arg = (struct pthread_args*) ptr;
-
-
-	char current_word[20] = "";
-	int c = 0;
-
-	pthread_mutex_lock(&mutex);
-		//Critical Region here
-
-//	for (int i=a; thread_arg->buffer[i]!=TERMINATOR; i++){
-
-//		if(isalpha(thread_arg->buffer[i]) && i%CHUNKSIZE!=0 ){
-//			current_word[c++] = thread_arg->buffer[i];
-//		} 
-
-//		else {
-//			current_word[c] = '\0';
-//			int res = getNameIndex(current_word); // new hash-function
-//			if (res != -1)
-// 				thread_arg->histogram[res]++;
-// 			c = 0;
-//		}
-//	a=i;
-//	}
-
-	pthread_mutex_unlock(&mutex);
-
-	return NULL;
-
+    while(1) {
+    
+        pthread_mutex_lock(&mutex_variable);
+        count = curTask_index;
+        curTask_index++;
+        pthread_mutex_unlock(&mutex_variable);
+        
+        if (count < (CHUNK-1)) {
+            start = count * CHUNKSIZE;
+            end = (count + 1) * CHUNKSIZE;
+        } else if (count == (CHUNK-1)) {
+            start = count * CHUNKSIZE;
+            end = count * CHUNKSIZE + residual;
+        } else
+            break;
+        
+        char current_word[20] = "";
+        int c = 0;
+        for (unsigned int i = start; i < end; i++) {
+                if (isalpha(global_buffer[i])) {
+	            current_word[c++] = global_buffer[i];
+	        } else {
+	            current_word[c] = '\0';
+	            int res = getNameIndex(current_word);
+	            if (res != -1)
+	                local_hist[res]++;
+	            c = 0;
+	        }
+        }
+    }
+    
+    for (int i=0; i<NNAMES; i++)
+    {
+    	m_hist[i] = local_hist[i];
+    }
+    
+    return NULL;
 }
 
-void get_histogram(char *buffer, int* histogram, int num_threads){
 
-	pthread_t *thread;
-	struct pthread_args *argument;
+void get_histogram(char *buffer, int* histogram, int num_threads) {
+    global_buffer = buffer;
+    CHUNK = strlen(buffer) / CHUNKSIZE;
+	curTask_index=0;
+	
+    pthread_t *thread;
+    thread = (pthread_t*)malloc(num_threads * sizeof(*thread));
 
-	thread=(pthread_t*)malloc(num_threads * sizeof(*thread));
-	argument=(struct pthread_args*)malloc(num_threads * sizeof(*argument));
-
-
-	for (int i=0;i<num_threads; i++){
-
-		argument[i].buffer=buffer;
-		argument[i].histogram=histogram;
-		
-		pthread_create(thread+i,NULL,&threadOperator,argument+i);
-	}
-
-	for (int i=0;i<num_threads; i++){
-		pthread_join(thread[i] , NULL);
-		
-	}
-
-	free(thread);
-	free(argument);
-
+	int** hist = (int**)malloc(num_threads*sizeof(int*));
+    for (int i=0; i<num_threads; i++)
+      	hist[i] = (int*)calloc(NNAMES,sizeof(int));
+    	
+    for (int i = 0; i < num_threads; i ++) 
+        pthread_create(thread+i, NULL,&ThreadWorker, hist[i]);
+ 
+    residual = 0;
+    
+    while (global_buffer[(CHUNK-1)*CHUNKSIZE+residual] != TERMINATOR)
+        residual++;
+        
+    for (int i = 0; i < num_threads; i ++)
+        pthread_join(thread[i], NULL);
+    
+	for (int i=0; i<num_threads; i++)
+        for (int j=0; j<NNAMES; j++)
+     	    histogram[j] += hist[i][j];
+    
+    free(thread);
 }
+
